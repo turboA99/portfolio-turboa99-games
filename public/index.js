f822d982d11471cd5fafb5bdfba617de4f23053c
Artur Skvorcov <turboRO99@gmail.com>
2024-01-17 00:09:04 +0100

    Fix 3

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 var isTouchDevice =
 	"ontouchstart" in document.documentElement ||
+	"ontouchstart" in window ||
 	navigator.maxTouchPoints > 0 ||
 	navigator.msMaxTouchPoints > 0;
 
 if (isTouchDevice) {
-	document.querySelector("custom-section > .white").remove();
+	document.querySelector(".white").style.display = "none";
 }
