e2dba55bbb76724133c497d8420a7563a55b11a6
Artur Skvorcov <turboRO99@gmail.com>
2024-01-17 00:19:24 +0100

    Fixed flickering of the flashlight

--- public/custom-elements.js
+++ public/custom-elements.js
@@ -4,8 +4,8 @@ class SectionElement extends HTMLElement {
 	}
 	connectedCallback() {
 		if (this.hasAttribute("mouse-over")) {
-			this.style.setProperty("--mouse-x", "0px");
-			this.style.setProperty("--mouse-y", "0px");
+			this.style.setProperty("--mouse-x", "-1000vw");
+			this.style.setProperty("--mouse-y", "-1000vh");
 			this.addEventListener("mousemove", (ev) => {
 				this.style.setProperty("--mouse-x", ev.clientX + "px");
 				this.style.setProperty("--mouse-y", ev.clientY + "px");
