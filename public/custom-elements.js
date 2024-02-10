270285499dbe365cc13a975855d1b719554db6f1
Artur Skvorcov <turboRO99@gmail.com>
2024-02-10 16:21:18 +0100

    debug test

--- public/custom-elements.js
+++ public/custom-elements.js
@@ -14,6 +14,8 @@ class SectionElement extends HTMLElement {
 			this.addEventListener("mousemove", (ev) => {
 				this.mousePos.x = ev.clientX;
 				this.mousePos.y = ev.clientY;
+				this.style.setProperty("--mouse-x", this.mousePos.x);
+				this.style.setProperty("--mouse-y", this.mousePos.y);
 				this.targetPos = this.mousePos;
 				if (!this.isMoveRunning) this.moveToTarget();
 			});
