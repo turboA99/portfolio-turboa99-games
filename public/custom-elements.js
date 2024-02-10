7b122dd0ab6e5dddd54ed445cf339fa91fb63d68
Artur Skvorcov <turboRO99@gmail.com>
2024-02-10 16:11:57 +0100

    Natural mouse movement var change

--- public/custom-elements.js
+++ public/custom-elements.js
@@ -1,16 +1,35 @@
 class SectionElement extends HTMLElement {
 	constructor() {
+		this.mousePos = { x: 0, y: 0 };
+		this.targetPos = this.mousePos;
+		this.curPos = this.targetPos;
+		this.speedThreashold = 20;
+		this.isMoveRunning = false;
 		super();
 	}
 	connectedCallback() {
 		if (this.hasAttribute("mouse-over")) {
-			this.style.setProperty("--mouse-x", "-1000vw");
-			this.style.setProperty("--mouse-y", "-1000vh");
+			this.style.setProperty("--mouse-x", this.mousePos.x);
+			this.style.setProperty("--mouse-y", this.mousePos.y);
 			this.addEventListener("mousemove", (ev) => {
-				this.style.setProperty("--mouse-x", ev.clientX + "px");
-				this.style.setProperty("--mouse-y", ev.clientY + "px");
+				this.mousePos.x = ev.clientX;
+				this.mousePos.y = ev.clientY;
+				this.targetPos = this.mousePos;
+				if (!this.isMoveRunning) this.moveToTarget();
 			});
 		}
+	}
+	async moveToTarget() {
+		this.isMoveRunning = true;
+		while (this.curPos != this.targetPos) {
+			this.curPos +=
+				this.targetPos - this.curPos > this.speedThreashold
+					? this.speedThreashold
+					: this.targetPos - this.curPos;
+			this.style.setProperty("--mouse-x", this.curPos.x);
+			this.style.setProperty("--mouse-y", this.curPos.y);
+		}
+		this.isMoveRunning = false;
 	}
 }
 
