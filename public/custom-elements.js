15c85774142a4e5a5c367c5050d080ab2e7e84b5
Artur Skvorcov <turboRO99@gmail.com>
2024-02-10 17:03:57 +0100

    HUJ

--- public/custom-elements.js
+++ public/custom-elements.js
@@ -5,7 +5,7 @@ class SectionElement extends HTMLElement {
 			this.mousePos = { x: 0, y: 0 };
 			this.targetPos = this.mousePos;
 			this.curPos = this.targetPos;
-			this.speedThreashold = 20;
+			this.speedThreashold = 2;
 			this.isMoveRunning = false;
 			this.style.setProperty("--mouse-x", this.curPos.x + "px");
 			this.style.setProperty("--mouse-y", this.curPos.y + "px");
@@ -17,11 +17,12 @@ class SectionElement extends HTMLElement {
 				this.mousePos.x = ev.clientX;
 				this.mousePos.y = ev.clientY;
 				this.targetPos = this.mousePos;
-				if (!this.isMoveRunning) Promise.resolve(this.moveToTarget());
+				if (!this.isMoveRunning) this.moveToTarget();
 			});
 		}
 	}
 	async moveToTarget() {
+		console.log(this);
 		this.isMoveRunning = true;
 		console.log(this.isMoveRunning);
 		while (this.curPos != this.targetPos) {
