6ab40d04277e58388b5ad9d36e558f8f15279fe8
Artur Skvorcov <turboRO99@gmail.com>
2025-05-03 00:38:46 +0200

    Checking how transitions work

--- public/index.js
+++
@@ -1,9 +0,0 @@
-var isTouchDevice =
-	"ontouchstart" in document.documentElement ||
-	"ontouchstart" in window ||
-	navigator.maxTouchPoints > 0 ||
-	navigator.msMaxTouchPoints > 0;
-
-if (isTouchDevice) {
-	document.querySelector("custom-section > .white").remove();
-}
